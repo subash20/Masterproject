79ed4877698d8de7d6d19013b24bb9d21d66628e
subash20 <medawadisubash@gmail.com>
2017-03-22 15:31:51 +0100

    Add files via upload

--- project.py
+++ project.py
@@ -1,47 +1,78 @@
 #!/usr/bin/env python
 
-import sys,re
-import MySQLdb
-from bs4 import BeautifulSoup
-from database import data
-
-#db=MySQLdb.connect("localhost","root","raspberry","clientinfo")
+import sys,MySQLdb
+import schedule
+from scapy.all import *
 
 def banner():
-	print " ################################"
-	print "     WiFI Packet Parser     "
-	print	"##############################"
+	print "##################################"
+	print " Finding AP and mac"
+	print "####################################"
 
 
-def parser():
-	count=1
-	html=open(sys.argv[1],'r').read()
-	soup=BeautifulSoup(html)
-	#for time in soup.find_all("wireless-client"):
-	#	print "Time of connection:"+time.get("last-time")
-	for link in soup.find_all('wireless-client'):
-		#print "Time of connection:"+link.get("last-time")
-		time=link.get("last-time")
+apmac=set()
+clientmac=set()
+def handler(pkt):
+	if pkt.haslayer(Dot11ProbeReq):
+		if pkt.info:
+			value=pkt.addr2+"---"+pkt.info
+			if value not in clientmac:
+				clientmac.add(value)
+				
+				#print "client MAC "+pkt.addr2 +"   "+"SSID :"+pkt.info
+
+				currenttime=time.ctime()
+                                conn_time=re.findall(r'([0-9]{2}\:[0-9]{2}\:[0-9]{2})',currenttime)
+                                connectiontime=str(conn_time[0])
+				db=MySQLdb.connect("localhost","root","raspberry","Probrequest")
+				cursor=db.cursor()
+				db=MySQLdb.connect("localhost","root","raspberry","Probrequest")
+				cursor=db.cursor()
+                                x=pkt.notdecoded
+                                try:
+                                        signalstrength=struct.unpack('>b',x[-2:-1])[0]    # RSSI bit 16 of radiotapheader
+					sql1="insert into clientinfo(clientmac,signalstrengthdbm,connectiontime,SSID) values('%s','%d','%s','%s');"%(pkt.addr2,signalstrength,currenttime,pkt.info)
+					cursor.execute(sql1)
+					db.commit()
+						
+					print "client MAC "+pkt.addr2 +"   "+"SSID :"+pkt.info
+                                        print connectiontime+"        "+str(signalstrength)+ "dbm"
+                                except IndexError:
+                                        	print " Index error out of range"
+
+				db.close()
+
+def read():
+	db=MySQLdb.connect("localhost","root","raspberry","Probrequest")
+	cursor=db.cursor()
+	sql="select clientmac,SSID from clientinfo;"
+        cursor.execute(sql)
+        results=cursor.fetchall() #reading from databases
+        if not results:
+        	print " Databases is empty"
+        else:
+		print " *****************************"
+               	for row in results:
+                	client=row[0]
+                        ssid=row[1]
+			data=client+"---"+ssid
+			if data not in apmac:                
+                        	apmac.add(data)
+				print data
+                                                #print data
+	db.close()
+
 		
-		conn_time=re.findall(r'([0-9]{2}\:[0-9]{2}\:[0-9]{2})',time)
-		connectiontime= str(conn_time[0])
-		print connectiontime
-		clientmac= link.find("client-mac").text
-		signalstrength=link.find("max_signal_dbm").text
-		print clientmac + " "+ signalstrength
-		sigstr=int(signalstrength)
-		data().inserti(clientmac,sigstr,connectiontime)
-		
-		#cursor=db.cursor()
-		
-		#try:			
-		#	cursor.execute("insert into client(clientmac,signalstrength,connectiontime) values('%s','%d','%s');",%(clientmac,sigstr,connectiontime))
-		#	db.commit()
-		#except MySQLdb.InterfaceError,e :
-		#	print "error in database"
-		#	raise e
-		#	db.rollback()
-	#db.close()	
+
+
+
+
+
+
 
 banner()
-parser()
+read()
+#sniff(iface="mon0",count=int(sys.argv[1]),prn=handler)
+
+sniff(iface="mon0",count=5000,prn=handler)
+		
